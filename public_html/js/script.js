11f356f6ef803a3185d8f3feedd23efe8ac10f98
DakotaD <dakota.degracia@gmail.com>
2015-01-20 14:24:11 -0800

    add a moving background image and a color behind my text on my jquery page

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,5 +1,59 @@
 
 $("document").ready(function(){
-     $(".bruh").css("background-color", "yellow");    
+     $('.bruh').css('background-color','yellow');    
     
+    $('#superHumans').accordion({header: "h3"});
+    
+    $('p:last').css("background-color",'green');
+    
+    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    
+    $('#show').css('visibility', 'hidden');
+
+$('#hide').bind('click', hideThepage);
+    
+    $('#show').bind('click', showThepage);
+    
+    $('h1').bind('click',mouseClick);
+    
+    $('#replaceWtext').bind('click',replaceWtext);
+    
+    $("#randPara").bind('click',addAPara);
+    
+    $('#removePara').bind('click', removeAPara);
 });
+function hideThepage(){
+    $('show').css('visibility', 'visible');
+    $('div').hide('slide', {}, 2500);
+    $("#show").show('fold', {}, 2500);
+}
+
+function showThepage(){
+    $('div').show('fold', {}, 2500);
+    $('show').show('puff', {}, 2500);
+    
+}
+
+function removeAPara(){
+    $('#randPara p:last').remove();
+}
+
+function addAPara(){
+    $('#randPara').append('<p>ADDED</P>');
+}
+function replaceWtext(){
+ 
+    $('#replaceWtext').text("<h1> hi my name is bob</h1>");
+ 
+}
+function mouseOutMe(){
+
+ 
+    ('h1').html("I am the best");
+  }
+  
+  function mouseOverMe(){
+      
+      $('p').html('My Clash of clans base is the best');
+  }
+
